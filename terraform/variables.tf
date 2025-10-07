78c66950bdf5a5e646c7f426aa997e8ad2b54d6d
bijukurup <seebiju@gmail.com>
2025-10-07 22:00:24 +0800

    variable update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,91 @@
-variable "prefix"             { type = string }
-# Singapore region
+variable "prefix" {
+  type = string
+}
+
 variable "location" {
   type    = string
   default = "southeastasia"
 }
 
-variable "node_count"         { type = number  default = 2 }
-variable "node_size"          { type = string  default = "Standard_D4s_v5" }
-variable "dns_prefix"         { type = string  default = null }
-variable "kubernetes_version" { type = string  default = null }
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+variable "node_size" {
+  type    = string
+  default = "Standard_D4s_v5"
+}
+
+variable "dns_prefix" {
+  type    = string
+  default = null
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = null
+}
 
 # Networking
-variable "vnet_cidr"          { type = string  default = "10.50.0.0/16" }
-variable "subnet_aks"         { type = string  default = "10.50.1.0/24" }
-variable "subnet_appgw"       { type = string  default = "10.50.2.0/24" }
+variable "vnet_cidr" {
+  type    = string
+  default = "10.50.0.0/16"
+}
+
+variable "subnet_aks" {
+  type    = string
+  default = "10.50.1.0/24"
+}
+
+variable "subnet_appgw" {
+  type    = string
+  default = "10.50.2.0/24"
+}
 
 # Private AKS API server
-variable "private_cluster"    { type = bool    default = true }
+variable "private_cluster" {
+  type    = bool
+  default = true
+}
 
 # ACR
-variable "acr_sku"            { type = string  default = "Standard" }
+variable "acr_sku" {
+  type    = string
+  default = "Standard"
+}
 
 # AGIC / App Gateway
-variable "appgw_sku"          { type = string  default = "WAF_v2" }
-variable "appgw_capacity"     { type = number  default = 2 }
-variable "appgw_public"       { type = bool    default = true } # public frontend; set false for private
-variable "argocd_host"        { type = string  default = "argocd.corp.example.com" }
+variable "appgw_sku" {
+  type    = string
+  default = "WAF_v2"
+}
+
+variable "appgw_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "appgw_public" {
+  type    = bool
+  default = true
+}
+
+variable "argocd_host" {
+  type    = string
+  default = "argocd.corp.example.com"
+}
 
 # Argo CD SSO (AAD via Dex)
-variable "aad_client_id"      { type = string }
-variable "aad_client_secret"  { type = string }
-variable "aad_tenant_id"      { type = string }
+variable "aad_client_id" {
+  type = string
+}
+
+variable "aad_client_secret" {
+  type = string
+}
+
+variable "aad_tenant_id" {
+  type = string
+}
+
